562ab69a971ce6e37119c28ebafe502aa1bff337
Hentioe <meow.i5.br@gmail.com>
2024-02-06 04:19:51 +0800

    Support Palworld startup parameters ​

--- entrypoints/palworld-server.sh
+++ entrypoints/palworld-server.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-steamcmd +force_install_dir \
-    steamapps/common/PalServer +login anonymous +app_update 2394010 validate +quit
+ROOT_DIR=steamapps/common/PalServer
 
-SAVED_CONFIG_DIR=steamapps/common/PalServer/Pal/Saved/Config/LinuxServer
+steamcmd +force_install_dir $ROOT_DIR +login anonymous +app_update 2394010 validate +quit
+
+SAVED_CONFIG_DIR=$ROOT_DIR/Pal/Saved/Config/LinuxServer
 SETTINGS_FILE=$SAVED_CONFIG_DIR/PalWorldSettings.ini
 
 if [[ ! -f $SETTINGS_FILE ]]; then
@@ -18,7 +19,11 @@ if [[ "$SETTINGS_CONTENT" =~ ^[[:space:]]*$ ]]; then
     # 配置文件不存在或内容为空，复制默认配置。
     echo "Copying default config file..."
 
-    cp steamapps/common/PalServer/DefaultPalWorldSettings.ini $SETTINGS_FILE
+    cp $ROOT_DIR/DefaultPalWorldSettings.ini $SETTINGS_FILE
 fi
 
-exec steamapps/common/PalServer/PalServer.sh
+if [ $# -gt 0 ]; then
+    ARGS="$@"
+fi
+
+exec $ROOT_DIR/PalServer.sh "$ARGS"
